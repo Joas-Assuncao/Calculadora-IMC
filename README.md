acaecbb5f0232c26aa86c7cde9485221f347b7ef
Joas <joassalvior@gmail.com>
2021-05-18 22:38:58 -0300

    Calculadora de IMC finalizada

---
+++ README.md
@@ -0,0 +1,14 @@
+# Calculadora de IMC
+
+Bom, estava procurando ideias de projetos para treinar meu JavaScript e encontrei este na web. Fui fazer e saiu melhor do que eu esperava!
+
+## Como ocorreu:
+
+Fiz o design (inspirado no da web) e mudei cores e medidas para a que me agradava melhor... Utilizei HTML e CSS puro
+
+![Imagem da Calculadora](calc_imc.jpeg =300x250)
+
+Após conseguir deixar como queria, fui para a mente da coisa.
+Como a fórmula é pequena, apenas peguei os inputs, armazenei na variável, validei os dados e joguei na fórmula. Utilizei Math.round no resultado para não ficar muito confuso.
+
+![Imagem do Código](cod_calc_imc.jpeg =300x250)
---
+++ scripts.js
@@ -0,0 +1,21 @@
+function calcular() {
+    const txtName = document.querySelector("input#name");
+    const nmbHeight = document.querySelector("input#height");
+    const nmbWeight = document.querySelector("input#weight");
+    var res = document.querySelector("p#text");
+    
+    if(txtName.value.length === 0 || nmbHeight.value.length === 0 || nmbWeight.value.length === 0) {
+        alert('Complete todos os dados!')
+    } else {
+        var name = String(txtName.value);
+        var height = parseFloat(nmbHeight.value);
+        var weight = Number(nmbWeight.value);
+
+        
+        var imc = Math.round(weight / (height * height));
+        res.innerText = (`
+            ${name}, você tem ${height} de altura e ${weight} de peso, então seu IMC é de ${imc}.
+            O IMC ideal fica entre 18,5 e 24,9.
+        `);
+    }
+}
